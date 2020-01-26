8e17fab0d7a43473a888d17d396f57207f544d9a
John <john.19col@gmail.com>
2020-01-26 16:31:21 -0500

    Add react-shards dependency

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const App: React.FC = () => {
-  return <h1>Trial Messenger</h1>;
-};
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'shards-ui/dist/css/shards.min.css';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Trial Messenger</h1>
+    </div>
+  );
+};
